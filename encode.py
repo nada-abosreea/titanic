bb185ddd0a020abf61618128886f34dfb2511598
Nada Essam <49698516+nada-abosreea@users.noreply.github.com>
2021-06-22 10:14:35 +0200

    Delete encode.py

--- encode.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def one_hot_encode(df):
-	l_sex_dummies = pd.get_dummies(df['Sex'], drop_first=True)
-	df1 = pd.concat([df,l_sex_dummies],axis=1)
-	df.drop(['Sex'],axis=1,inplace=True)
-	
-	return df1
